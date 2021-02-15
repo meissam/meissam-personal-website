caef9bf2aa5fb9224875f844c878d88934d13174
Meissam Rasouli <meissam.rasouli@gmail.com>
2021-02-16 01:35:13 +0330

    change the layput of header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,18 @@
 /** @jsx jsx */
 import { jsx, useColorMode } from "theme-ui"
+import { Link } from "gatsby"
 import { Flex } from "@theme-ui/components"
 import useMinimalBlogConfig from "../hooks/use-minimal-blog-config"
 import ColorModeToggle from "./colormode-toggle"
 import Navigation from "./navigation"
 import HeaderTitle from "./header-title"
-import HeaderExternalLinks from "./header-external-links"
+
+import replaceSlashes from "../utils/replaceSlashes"
+import useSiteMetadata from "../hooks/use-site-metadata"
 
 const Header = () => {
-  const { navigation: nav } = useMinimalBlogConfig()
+  const { siteTitle, siteImage } = useSiteMetadata()
+  const { navigation: nav, basePath } = useMinimalBlogConfig()
   const [colorMode, setColorMode] = useColorMode()
   const isDark = colorMode === `dark`
   const toggleColorMode = (e: any) => {
@@ -19,25 +23,33 @@ const Header = () => {
   return (
     <header sx={{ mb: [5, 6] }}>
       <Flex sx={{ alignItems: `center`, justifyContent: `space-between` }}>
-        <HeaderTitle />
-        <ColorModeToggle isDark={isDark} toggle={toggleColorMode} />
+
+        <Flex sx={{ alignItems: `center`, justifyContent: `space-between` }}>
+          <img src={siteImage} alt={siteTitle} />
+          <HeaderTitle />
+        </Flex>
+
+        <div className="main-menu" 
+          sx={{
+            boxSizing: `border-box`,
+            display: `flex`,
+            margin: 0,
+            variant: `dividers.bottom`,
+            alignItems: `center`,
+            justifyContent: `space-between`,
+            mt: 3,
+            color: `secondary`,
+            a: { color: `secondary`, ":hover": { color: `heading` } },
+            flexFlow: `wrap`,
+          }}
+        >
+          <Navigation nav={nav} />
+          <ColorModeToggle isDark={isDark} toggle={toggleColorMode} />
+        </div>
+
+
       </Flex>
-      <div
-        sx={{
-          boxSizing: `border-box`,
-          display: `flex`,
-          variant: `dividers.bottom`,
-          alignItems: `center`,
-          justifyContent: `space-between`,
-          mt: 3,
-          color: `secondary`,
-          a: { color: `secondary`, ":hover": { color: `heading` } },
-          flexFlow: `wrap`,
-        }}
-      >
-        <Navigation nav={nav} />
-        <HeaderExternalLinks />
-      </div>
+
     </header>
   )
 }
